577079563b037b5b9dcf265e2103565785216eda
HeleneRamp <h.rampelberg@yahoo.fr>
2024-07-01 17:18:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1>Hello World !</h1>
+  </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+h1{
+  @include flex-center;
+  font-weight: bolder;
+  color: darkblue;
+}
+
+</style>
